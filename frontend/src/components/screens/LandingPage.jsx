79fc51b1ec0da4fcd51d1f45d013df442ac9a63b
Jaay7 <jayakrishna.madem@gmail.com>
2022-06-07 16:03:00 +0530

    navigate

--- frontend/src/components/screens/LandingPage.jsx
+++ frontend/src/components/screens/LandingPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom';
 
 const LandingPage = () => {
+
+  if(localStorage.getItem('token')) {
+    return <Navigate to="/home" />
+  }
+
   return (
     <div>LandingPage</div>
   )
